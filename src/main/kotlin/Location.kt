36021fd9f81145b0388f7b082b4a1981ca2601ba
Paul Martin <communty.hivemind@gmail.com>
2019-08-15 13:00:33 +0100

    Define Purchasable, Rentable interfaces for Location

--- src/main/kotlin/Location.kt
+++ src/main/kotlin/Location.kt
@@ -5,19 +5,25 @@ sealed class Location {
         val bonus = GBP(100)
     }
 
-    class FactoryOrWarehouse(val name: String) : Location() {
-        val cost = GBP(100)
+    class FactoryOrWarehouse(val name: String) : Location(), Purchasable, Rentable {
+        override val cost = GBP(100)
         val rent = GBP(20)
     }
 
     class Retail(
         val name: String,
-        val cost: GBP,
+        override val cost: GBP,
         val group: Group,
         val undeveloped: DevelopmentType.RentOnly,
         val ministore: DevelopmentType.CostAndRent,
         val supermarket: DevelopmentType.CostAndRent,
         val megastore: DevelopmentType.CostAndRent
-    ) : Location()
+    ) : Location(), Purchasable, Rentable
+
+    interface Rentable
+
+    interface Purchasable {
+        val cost: GBP
+    }
 
 }
