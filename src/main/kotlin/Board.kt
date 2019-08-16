90ab1ee67f8e5840613aa0a4b733e24fdc8d0dab
Paul Martin <communty.hivemind@gmail.com>
2019-08-16 15:02:02 +0100

    Refactor Board.doesMovePassGo method
    Co-authored-by: Amy Jordan <amy.jordan@johnlewis.co.uk>

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,13 +1,10 @@
 class Board(val locations: List<Location>) {
-    fun doesMovePassGo(start: Int, spaces: Int): Boolean {
-        val startingSpace = start + 1
-        val endingSpace = (start + spaces) % locations.size
+    fun doesMovePassGo(startingSpace: Int, spaces: Int): Boolean {
+        val checkFrom = startingSpace + 1
+        val checkTo = startingSpace + spaces
 
-        return if (endingSpace > startingSpace) {
-            locations.slice(startingSpace..endingSpace).contains(Location.Go)
-        } else {
-            locations.slice(startingSpace..locations.size - 1).contains(Location.Go) ||
-                   locations.slice(0..endingSpace).contains(Location.Go)
-        }
+        val spacesVisited = (checkFrom..checkTo).toSet().map { it % locations.size }
+        return locations.slice(spacesVisited).contains(Location.Go)
+
     }
 }
