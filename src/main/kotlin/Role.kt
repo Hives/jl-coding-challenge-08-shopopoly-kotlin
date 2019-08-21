f5150006092e06fe8ff1e506657676b0704b0c84
Paul Martin <paul.y.martin@johnlewis.co.uk>
2019-08-21 14:46:48 +0100

    Update Player.move to take a DiceRoll

--- src/main/kotlin/Role.kt
+++ src/main/kotlin/Role.kt
@@ -4,7 +4,7 @@ object Bank : Role()
 
 class Player(val name: String, val board: Board) : Role() {
     var boardLocation = 0
-    fun move(spaces: Int) {
-        boardLocation = (boardLocation + spaces) % board.locations.size
+    fun move(roll: DiceRoll) {
+        boardLocation = (boardLocation + roll.total()) % board.locations.size
     }
 }
--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class PlayerTest {
-    val board = Board(List(20) { Location.FreeParking })
+    val board = Board(List(13) { Location.FreeParking })
     @Test
     fun `player starts on first square`() {
         val player = Player("Paul", board)
@@ -11,14 +11,15 @@ internal class PlayerTest {
     @Test
     fun `player can move 2 spaces` () {
         val player = Player("Paul", board)
-        player.move(2)
+        player.move(DiceRoll(1, 1))
         assertEquals(2,player.boardLocation)
     }
 
     @Test
     fun `player can wrap around the board`() {
         val player = Player("Paul", board)
-        player.move(21)
+        player.move(DiceRoll(6, 6))
+        player.move(DiceRoll(1, 1))
         assertEquals(1, player.boardLocation)
     }
 
